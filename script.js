b6f048735ef5b62b23a982658146832756bb1767
John A. V. Cinquegrana <jcinqueg@stevens.edu>
2020-05-18 16:11:05 -0400

    generated a skeleton for the project

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * @name        script.js
+ * @author      John Cinquegrana, <your name here>
+ * @date        5/18/2020
+ * @abstract    Uses the Spotify API to recommend songs based on mood
+ */
+
+function formHandler() {
+    var input = document.getElementById("input").value;
+    console.log( "Input read: " + input );
+}
