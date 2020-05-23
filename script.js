6acf23543b325be1656b474eb64272b0a3a5d894
John A. V. Cinquegrana <jcinqueg@stevens.edu>
2020-05-22 21:04:32 -0400

    version 1.0 finished

--- script.js
+++ script.js
@@ -13,12 +13,28 @@ console.log("Loading script.js.");
 function reccomend() {
     var input = document.getElementById("input").value;
     console.log( "Input read: " + input );
+    var song = getRecommendation();
+    Document.getElementById("output").innerHTML = song.toString();
 }
 
 /**
- * 
- * @param {string} song 
+ * Gets a proper recommendation for someones tastes in music.
+ * Warning, technically only the toString() method is safe incase somehow the user is not recommended
+ * any songs.
  */
-function getMoodLib(song) {
-
+function getRecommendation() {
+  //window.popSongs holds all of the popular songs
+  var keySong = getKeySong();
+  var minstance = 10;
+  var targetSong = new Object();
+  targetSong.toString = function () { return "Sorry, you're tastes are inconceivable. We have no song for you." };
+  for( song in window.popSongs ) {
+    //For each song, check it's distance and update variables as necessary
+    var distance = songDistance( keySong, song );
+    if( distance < minstance ) {
+      minstance = distance;
+      targetSong = song;
+    }
+  }
+  return targetSong;
 }
